a320f106a34236eeba1a14679abc5cd9940535a1
begzodmominov2009 <begzodmominov32@gmail.com>
2025-10-31 00:35:22 +0300

    adding carusel

---
+++ js/carusel.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        },
+    },
+});
